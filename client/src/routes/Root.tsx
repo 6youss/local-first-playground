6f800cfb5bc328a64838d6fd3de4bb4725ad1d95
6youss <youcef.zahariou@gmail.com>
2024-08-21 13:04:36 +0200

    add update screen

--- client/src/routes/Root.tsx
+++ client/src/routes/Root.tsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 export function Root() {
-  const [notes, setNotes] = useState<{ id: number; title: string; content: string }[]>([]);
+  const [notes, setNotes] = useState<
+    { id: number; title: string; content: string }[]
+  >([]);
 
   useEffect(() => {
     getNotes();
@@ -10,7 +12,9 @@ export function Root() {
 
   async function getNotes() {
     try {
-      const res = await fetch("http://localhost:3000/notes", { headers: { "content-type": "application/json" } });
+      const res = await fetch("http://localhost:3000/notes", {
+        headers: { "content-type": "application/json" },
+      });
       const json = await res.json();
       if (!res.ok) {
         throw new Error(json);
@@ -24,13 +28,20 @@ export function Root() {
   return (
     <div className="flex justify-start min-h-screen">
       <nav id="side-nav" className="flex flex-col bg-gray-100 gap-4 pt-4">
-        {notes.map((n) => {
-          return (
-            <Link to={`/update-note/${n.id}`} className="container mx-auto flex justify-between items-center">
-              <span>{n.title}</span>
-            </Link>
-          );
-        })}
+        <ul>
+          {notes.map((n) => {
+            return (
+              <li key={n.id}>
+                <Link
+                  to={`/update-note/${n.id}`}
+                  className="container mx-auto flex justify-between items-center"
+                >
+                  <span>{n.title}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
       </nav>
       <div className="flex-grow">
         <Outlet />
